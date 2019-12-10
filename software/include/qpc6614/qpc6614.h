ad530647ad81c68bd493373b36b70550c1ae5829
Abel Jimenez <abelj1@uci.edu>
2019-12-10 01:07:41 +0000

    fixed qpc6614

--- software/include/qpc6614/qpc6614.h
+++ software/include/qpc6614/qpc6614.h
@@ -1,20 +1,21 @@
 // qpc6614
-#ifdef __QPC6614_H__
-#define __QPC6614_H__
+#ifndef QPC6614_H
+#define QPC6614_H
 
 #include <stdint.h>
 
 class QPC6614
 {
-    uint8_t LE_GPIO;
-    uint8_t CLK_GPIO;
-    uint8_t DATA_GPIO;
-    uint8_t CE_GPIO;
+    int LE_GPIO;
+    int CLK_GPIO;
+    int DATA_GPIO;
+    int CE_GPIO;
 public:
-    QPC6614(void);
-    void set_db(int mode):
+    QPC6614();
+    void set_db(int mode);
 private:
-    void turn_on_wpi(void);
-    uint32_t write_qpc6614(uint32_t data);
+    void turn_on_wpi();
+    void write_qpc6614(uint32_t data);
 };
-#endif 
+
+#endif
--- software/src/qpc6614/qpc6614.cpp
+++ software/src/qpc6614/qpc6614.cpp
@@ -1,76 +1,18 @@
-#include <stdint.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <linux/types.h>
 #include <wiringPi.h>
-
-#include "tca9555/tca9555.h"
+#include <unistd.h>
 #include "qpc6614/qpc6614.h"
+#include "tca9555/tca9555.h"
 
-QPC6614::QPC6614(void)
+QPC6614::QPC6614()
 {
-    LE_GPIO = 30; // doesnt matter
-    CE_GPIO = 23; // doesnt matter
+    // Doesnt Matter
+    LE_GPIO = 0;
+    CE_GPIO = 0;
 
-    // wiring pi 
+    // Wiring Pi Pins
     CLK_GPIO = 14;
     DATA_GPIO = 25;
     turn_on_wpi();
-}
-
-void QPC6614::turn_on_wpi(void)
-{
-    if(wiringPiSetup() == -1);
-}
-
-int32_t QPC6614::write_qpc6614(uint32_t data)
-{
-    // Set LE to Output 
-    TCA9555 tca(0x20);
-    tca.setPortDirection(1,1,0);
-    tca.setOutputStates(1,1,1);
-
-	pinMode(CLK_GPIO, OUTPUT);
-	pinMode(DATA_GPIO, OUTPUT);
-
-	usleep(10);
-    // Set LE LOW
-	tca.setOutputStates(1,1,0);
-	usleep(10);
-
-	uint16_t i;
-
-	for(i = 0; i < 6; i++)
-	{
-		if(data & 0x20)
-		{
-			digitalWrite(DATA_GPIO, HIGH);
-		}
-		else
-		{
-			digitalWrite(DATA_GPIO, LOW);
-		}
-		
-		usleep(10);
-		digitalWrite(CLK_GPIO, HIGH);
-		usleep(10);
-		digitalWrite(CLK_GPIO, LOW);
-		usleep(10);
-
-		data <<= 1;	
-	}
-	
-    // Set LE to HIGH
-	tca.setOutputStates(1,1,1);
-	usleep(10);
-
-	return 0;
-
 }
 
 void QPC6614::set_db(int mode)
@@ -83,47 +25,90 @@ void QPC6614::set_db(int mode)
     // Mode 5 - 8dB
     // Mode 6 - 16dB
     // Mode 7 - 31.5dB
-    
-	if(mode >= 0 && mode <=7)
-	{
-		if(mode == 0)
-		{
-			write_qpc6614(0b111111);
-		}
-		else if(mode == 1)
-		{
-			write_qpc6614(0b111110);
-		}
-		else if(mode == 2)
-		{
-			write_qpc6614(0b111101);
-		}
-		else if(mode == 3)
-		{
-			write_qpc6614(0b111011);
-		}
-		else if(mode == 4)
-		{
-			write_qpc6614(0b110111);
-		}
-		else if(mode == 5)
-		{
-			write_qpc6614(0b101111);
-		}
-		else if(mode == 6)
-		{
-			write_qpc6614(0b011111);
-		}
-		else if(mode == 7)
-		{
-			write_qpc6614(0b000000);
-		}
-	} 
+
+    // check input
+    if(mode < 0 && mode >7)
+    {
+        return;
+    }
+
+    if(mode == 0)
+    {
+        write_qpc6614(0b111111);
+    }
+    else if(mode == 1)
+    {
+        write_qpc6614(0b111110);
+    }
+    else if(mode == 2)
+    {
+        write_qpc6614(0b111101);
+    }
+    else if(mode == 3)
+    {
+        write_qpc6614(0b111011);
+    }
+    else if(mode == 4)
+    {
+        write_qpc6614(0b110111);
+    }
+    else if(mode == 5)
+    {
+        write_qpc6614(0b101111);
+    }
+    else if(mode == 6)
+    {
+        write_qpc6614(0b011111);
+    }
+    else if(mode == 7)
+    {
+        write_qpc6614(0b000000);
+    }
+
 }
 
-int main(void)
+void QPC6614::turn_on_wpi()
 {
-	return 0;
+    if(wiringPiSetup() == -1);
 }
 
+void QPC6614::write_qpc6614(uint32_t data)
+{
+    TCA9555 tca(0x20);
+    tca.setPortDirection(1,1,0);
+    tca.setPortDirection(1,1,1);
 
+    pinMode(CLK_GPIO, OUTPUT);
+    pinMode(DATA_GPIO, OUTPUT);
+
+    usleep(10);
+    //set LE LOW
+    tca.setOutputStates(1,1,0);
+    usleep(10);
+
+    uint16_t i;
+
+    for(i = 0; i < 6; i++)
+    {
+        if(data & 0x20)
+        {
+            digitalWrite(DATA_GPIO, HIGH);
+        }
+        else
+        {
+            digitalWrite(DATA_GPIO, LOW);
+        }
+
+        usleep(10);
+        digitalWrite(CLK_GPIO, HIGH);
+        usleep(10);
+        digitalWrite(CLK_GPIO, LOW);
+        usleep(10);
+
+        data <<= 1;
+    }
+
+    // Set LE HIGH
+    tca.setOutputStates(1,1,1);
+    usleep(10);
+}
