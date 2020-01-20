e7543f3309e00bde2b88019cb125859b003b2bf9
khoitd1997 <khoidinhtrinh@gmail.com>
2020-01-19 20:27:54 -0800

    added some code to main for testing

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -3,6 +3,17 @@
 #include "rffc5072/rffc5072.h"
 
 int main(){
+    rffc5071_driver_t rffc5071 = {
+        .pin = {
+            .gpio_enx = 28,
+            .gpio_clock = 14,
+            .gpio_data = 29,
+        }
+    };
+
+    rffc5071_setup(&rffc5071);
+    rffc5071_set_frequency(&rffc5071, 90);
+
     TCA9555 tca(0x20);
     return 0;
 }
